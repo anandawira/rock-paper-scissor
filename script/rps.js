a34458fcce9e5d6cb792506ef297d5d332549963
Ananda Wiradharma <anandawira754@gmail.com>
2020-09-29 22:01:38 +0700

    add GUI for play

---
+++ script/rps.js
@@ -0,0 +1,15 @@
+const options = ["Rock", "Paper", "Scissors"];
+function computerPlay() {
+  const index = Math.floor(Math.random() * options.length);
+  return index;
+}
+
+function playRound(playerSelectionIndex, computerSelectionIndex) {
+  if (playerSelectionIndex == computerSelectionIndex) {
+    return `Its a Draw!, you both picked ${options[playerSelectionIndex]}`;
+  } else if ((playerSelectionIndex + 1) % 3 == computerSelectionIndex) {
+    return `You Lose! ${options[computerSelectionIndex]} beats ${options[playerSelectionIndex]}`;
+  } else {
+    return `You Win! ${options[playerSelectionIndex]} beats ${options[computerSelectionIndex]}`;
+  }
+}
